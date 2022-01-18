9e8312619c2446fb3195672110c6452117e710ee
Hiroto Yagi <pppiroto@gmailcom>
2022-01-18 21:15:25 +0900

    Wijmo カレンダーサンプル

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { WjInputModule } from '@grapecity/wijmo.angular2.input';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -26,6 +27,7 @@ import { PlaygroundComponent } from './playground/playground.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    WjInputModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'main', component: MainFrameComponent },
