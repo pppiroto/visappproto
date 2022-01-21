c75acd87be7d04a6de10607240e505a34075f93a
Hiroto YAGI <pppiroto@gmail.com>
2022-01-22 01:16:52 +0900

    テストプロジェクトの作成とソリューションの作成及び追加

---
+++ README.md
@@ -0,0 +1,6 @@
+# ソリューションの作成とプロジェクトの追加
+```
+$ dotnet new sln -o .
+$ dotnet sln add visappproto
+$ dotnet sln add visappproto_test
+```
---
+++ visappproto_test/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace visappproto_test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
