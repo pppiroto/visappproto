f01f7b3a2e8cc5e0443203fe8f33d29d08dc51c6
Hiroto YAGI <pppiroto@gmail.com>
2022-01-22 12:46:21 +0900

    Wijmo Frex Grid に Oracle サンプルデータのEmployeesを表示

--- visappproto/ClientApp/src/app/components/employees/employees.component.ts
+++ visappproto/ClientApp/src/app/components/employees/employees.component.ts
@@ -1,6 +1,8 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Employee } from '../../models/employee';
+import * as wjcCore from '@grapecity/wijmo';
+import * as wjcGrid from '@grapecity/wijmo.grid';
 
 @Component({
   selector: 'app-employees',
@@ -10,11 +12,30 @@ import { Employee } from '../../models/employee';
 export class EmployeesComponent implements OnInit {
 
   public employees: Employee[] = [];
+  selectedItem: string = "";
+
+  @ViewChild('flex', { static: true })
+  flex!: wjcGrid.FlexGrid;
+
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<Employee[]>(baseUrl + 'ormdataaccess').subscribe(result => {
+    http.get<Employee[]>(baseUrl + 'employees').subscribe(result => {
       this.employees = result;
     }, error => console.error(error));
+  }
+
+  flexInitialized(flexgrid: wjcGrid.FlexGrid) {
+    // sort the data by country
+    let sd = new wjcCore.SortDescription('employeeId', true);
+    flexgrid.collectionView.sortDescriptions.push(sd);
+    flexgrid.collectionView.currentChanged.addHandler(this._updateCurrentInfo.bind(this));
+    this._updateCurrentInfo();
+  }
+
+  private _updateCurrentInfo() {
+      // this.selectedItem = wjcCore.format(
+      //     '国: <b>{country}</b>、売上: <b>{sales:c0}</b>、費用: <b>{expenses:c0}</b>',
+      //     this.flex.collectionView.currentItem);
   }
 
   ngOnInit(): void {
